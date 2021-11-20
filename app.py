7bf7d6cae98c535442820195e06ba444e308cc41
Bastyl <bastian.lopez@mail.udp.cl>
2021-11-20 20:10:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ from flask import request
 import datetime
 from flask import Flask
 
+import os
+import psycopg2
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
 app = Flask(__name__)
 
 @app.route("/")
