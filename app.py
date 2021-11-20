5a833db19a22e048664037678906ebc351049f89
Bastyl <bastian.lopez@mail.udp.cl>
2021-11-20 19:39:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+from flask import render_template,redirect,url_for
+from flask import request
+import datetime
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return render_template("index.html")
