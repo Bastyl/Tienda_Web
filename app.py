3cfe112a2f195948dbef8ae11275eadc0601f458
Bastyl <bastian.lopez@mail.udp.cl>
2021-11-23 19:47:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ app = Flask(__name__)
 
 #######
 #from config import Config
-#from flask_sqlalchemy import SQLAlchemy
-#from sqlalchemy import create_negine
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_negine
 #app.config.from_object(Config)
 #DB_URI = application.config['SQLALCHEMY_DATABASE_URI']
 #engine = create_engine(DB_URI)
