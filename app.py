1859690ccb465d5b41ff19bdf086338a6ae5e90d
Bastyl <bastian.lopez@mail.udp.cl>
2021-11-23 20:41:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -31,6 +31,16 @@ def register():
     engine.execute(account.insert(), username=username,
                    email=email, password=password_hash)
     return jsonify({'user_added': True})
+
+@application.route('/sign_in', methods=["GET", "POST"])
+def sign_in():
+    username_entered = request.args.get('username')
+    password_entered = request.args.get('password')
+    user = session.query(Accounts).filter(or_(Accounts.username == username_entered, Accounts.email == username_entered)
+                                          ).first()
+    if user is not None and check_password_hash(user.password, password_entered):
+        return jsonify({'signed_in': True})
+    return jsonify({'signed_in': False})
 ##
 
 
