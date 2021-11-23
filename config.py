6968183cb09c6ee73cbbef8d604f8db922e38d63
Bastyl <bastian.lopez@mail.udp.cl>
2021-11-23 20:57:44 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,15 @@
 import os
+from flask_login import UserMixin
+from app import db
+
 class Config(object):
 	SQLALCHEMY_TRACK_MODIFICATIONS = True
 	SECRET_KEY = os.urandom(24)
 	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+	
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
