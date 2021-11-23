22b6104e66c5f95f6ea6edfb947fe210544ee7b0
Bastyl <bastian.lopez@mail.udp.cl>
2021-11-23 20:38:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,17 @@ from sqlalchemy import create_engine
 app.config.from_object(Config)
 DB_URI = app.config['SQLALCHEMY_DATABASE_URI']
 engine = create_engine(DB_URI)
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    username = request.args.get('username')
+    email = request.args.get('email')
+    password = request.args.get('password')
+    password_hash = generate_password_hash(password)
+    account = Table('account', metadata, autoload=True)
+    engine.execute(account.insert(), username=username,
+                   email=email, password=password_hash)
+    return jsonify({'user_added': True})
 ##
 
 
