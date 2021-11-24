cb3dc88f0eb9abf256c1254a5d2c540c3a9e3d8d
Bastyl <bastian.lopez@mail.udp.cl>
2021-11-23 22:01:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,10 @@ db.init_app(app)
 login_manager = LoginManager()
 login_manager.login_view = 'app.login'
 login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 ##
 
 
