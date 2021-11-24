0215940ce6395bb942da8bff564c3b4dd808e5a5
Bastyl <bastian.lopez@mail.udp.cl>
2021-11-23 21:53:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ app.config.from_object(Config)
 db.init_app(app)
 login_manager = LoginManager()
 login_manager.login_view = 'app.login'
-#login_manager.init_app(app)
+login_manager.init_app(app)
 ##
 
 
