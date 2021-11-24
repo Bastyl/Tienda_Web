9e92f0b652a17f0c1c394425d0df67deb79a8dbc
Bastyl <bastian.lopez@mail.udp.cl>
2021-11-23 21:03:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,28 +21,10 @@ from flask_login import LoginManager,login_user,login_required, current_user, lo
 
 db = SQLAlchemy()
 app.config.from_object(Config)
-db.init_app(app)
-login_manager = LoginManager()
-login_manager.login_view = 'app.login'
-login_manager.init_app(app)
-
-@app.route('/login', methods=['POST'])
-def login_post():
-    email = request.form.get('email')
-    password = request.form.get('password')
-    remember = True if request.form.get('remember') else False
-
-    user = User.query.filter_by(email=email).first()
-
-    # check if the user actually exists
-    # take the user-supplied password, hash it, and compare it to the hashed password in the database
-    if not user or not check_password_hash(user.password, password):
-        flash('Please check your login details and try again.')
-        return redirect(url_for('app.login')) # if the user doesn't exist or password is wrong, reload the page
-
-    # if the above check passes, then we know the user has the right credentials
-    login_user(user, remember=remember)
-    return redirect(url_for('app.profile'))
+#db.init_app(app)
+#login_manager = LoginManager()
+#login_manager.login_view = 'app.login'
+#login_manager.init_app(app)
 ##
 
 
