135c3b065e3117d6b6b3eabd5d3abe9e35e6b5eb
Bastyl <bastian.lopez@mail.udp.cl>
2021-11-30 21:38:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,19 +6,86 @@ import psycopg2
 import os
 import base64
 
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+from models import *
+from flask_login import LoginManager,login_user,login_required, current_user, logout_user
+
+app = Flask(__name__)
+#app.secret_key = 'sa3iedoaoiopdia92jdkaojkASD@'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
 DATABASE_URL = os.environ['DATABASE_URL']
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cur = conn.cursor()
 
-app = Flask(__name__)
-
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
 @app.route('/index',methods=['POST','GET'])
 def index():
 	return render_template("index.html")
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+    # check if the user actually exists
+    # take the user-supplied password, hash it, and compare it to the hashed password in the database
+    if not user or not check_password_hash(user.password, password):
+        flash('Please check your login details and try again.')
+        return redirect(url_for('login')) # if the user doesn't exist or password is wrong, reload the page
+
+    # if the above check passes, then we know the user has the right credentials
+    login_user(user, remember=remember)
+    return redirect(url_for('index'))
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/signup', methods=['POST'])
+def signup_post():
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(email=email).first() 
+
+    if user: 
+        flash('Email address already exists')
+        return redirect(url_for('signup'))
+   
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for('login'))
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 @app.route('/ver_cojines',methods=['POST','GET'])
 def ver_cojines():
